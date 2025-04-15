4a0c0615683d4996e4b91f98e400d3803d8b44aa
zariness00 <zariness00@gmail.com>
2025-04-16 00:26:18 +0200

    Setting up a conda env

---
+++ Speech_tech/setup_conda_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
+conda create --name speech python=3.10 -y
+
+conda activate speech
+
+conda install -c conda-forge pip -y
+python -m pip install -r requirements.txt
+
+
+conda install scipy -y
+
+conda uninstall scipy -y
+
+python --version
+
+conda list --export > installed_packages.txt
